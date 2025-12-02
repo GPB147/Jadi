0eb6bc512584f8990c119aaafdcea4f1582b2533
Purple147 <gorcraft1380@gmail.com>
2025-12-02 10:16:10 +0330

    refactor: eular project 1

---
+++ src/jadi understanding programming/0 begginer/10.py
@@ -0,0 +1,13 @@
+# eular project 1
+def multiples(number):
+    return number % 3 == 0 or number % 5 == 0
+
+
+summing = 0
+for natural_numbers in range(1, 1000):
+    if multiples(natural_numbers):
+        summing += natural_numbers
+        print(natural_numbers)
+
+
+print(summing)
