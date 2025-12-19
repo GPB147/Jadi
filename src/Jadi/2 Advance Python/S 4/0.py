887b408cf9571132c20e615d0f0736b8e8fa5675
Purple147 <gorcraft1380@gmail.com>
2025-12-19 20:30:43 +0330

    Learning: ML

---
+++ src/Jadi/2 Advance Python/S 4/0.py
@@ -0,0 +1,11 @@
+# installing Lock libraries of my script by language(example: python) version in other server
+# Answer of this problem is Virtual Enviroment(Language version, pip version, libraries, libraries version)
+# Virtual Enviroment going to a Directory in other system Named Usaly venv
+# Making Virtual Enviroment Directory = python -m venv Directory name(Usaly .venv)
+# Activin Virtual Enviroment in Linux = source venv\bin\activate
+# Activin Virtual Enviroment in Windows = Directory name/scripts/activate
+# Deactivating Virtual Enviroment = deactivate
+# mkdir = making a folder, pip search x, pip show x, pip freeze, pip freeze > requirements.txt
+# Loock Library = Library==Version, cat Address
+# Virtual Enviroment + requirements.txt file = We good for sendin script to others 
+# for installing requirements.txt = pip install -r requirements.txt
